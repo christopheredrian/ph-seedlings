b5522c5d4e635479eadb8f19a6cf479a1c5ae943
Christopher Espiritu <christopheredrian@gmail.com>
2017-10-16 08:14:40 +0800

    Added Procfile for heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // For SQLSTATE 4200 error
+        \Schema::defaultStringLength(191);
     }
 
     /**
